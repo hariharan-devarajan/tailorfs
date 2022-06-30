b72d28625925d6de43ee021bbbd9d5a89ab959f8
Hariharan Devarajan <mani.hariharan@gmail.com>
2022-06-30 15:07:34 -0700

    write-only optimization

--- test/unifyfs/unifyfs_run_single.sh
+++ test/unifyfs/unifyfs_run_single.sh
@@ -64,7 +64,8 @@ status=$?
 echo "Killing UnifyFS daemon"
 echo "UNIFYFS_LOG_DIR=$UNIFYFS_LOG_DIR ${UNIFYFS_EXEC} terminate -c --share-dir=${PFS}"
 UNIFYFS_LOG_DIR=$UNIFYFS_LOG_DIR ${UNIFYFS_EXEC} terminate -c --share-dir=${PFS}
-jsrun -r 1 -a 1 `ps -aef | grep unifyfs | xargs kill -9`
+# shellcheck disable=SC2046
+jsrun -r 1 -a 1 `ps -aef | grep unifyfs | awk {'print $2'} | xargs kill -9`
 echo "Stopped unifyfs daemon. sleeping for ${SLEEP_TIME} seconds"
 sleep ${SLEEP_TIME}
 
