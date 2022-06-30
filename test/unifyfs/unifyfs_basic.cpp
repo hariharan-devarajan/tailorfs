7ea2c3abb8749fb01347c4133fd369dde00ffcaf
Hariharan Devarajan <mani.hariharan@gmail.com>
2022-06-29 18:16:13 -0700

    write-only optimization

--- test/unifyfs/unifyfs_basic.cpp
+++ test/unifyfs/unifyfs_basic.cpp
@@ -416,7 +416,7 @@ TEST_CASE("Read-Only", "[type=read-only][optimization=buffered_read]") {
             REQUIRE(read_req[i].result.count == args.request_size);
             for (int char_i = 0; char_i < args.request_size; ++char_i) {
               char val_c = ((char *)read_req[i].user_buf)[char_i];
-              fprintf(stderr, "index %d %c\n", char_i, val_c);
+              // fprintf(stderr, "index %d %c\n", char_i, val_c);
               REQUIRE(val_c == 'w');
             }
           }
