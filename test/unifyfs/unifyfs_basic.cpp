91ffb89b450e0d0a806aeaa902c0031c58afa4f6
Hariharan Devarajan <mani.hariharan@gmail.com>
2022-06-30 00:02:13 -0700

    write-only optimization

--- test/unifyfs/unifyfs_basic.cpp
+++ test/unifyfs/unifyfs_basic.cpp
@@ -284,8 +284,7 @@ TEST_CASE("Read-Only", "[type=read-only][optimization=buffered_read]") {
         "' > " + pfs_filename.u8string() + "_temp 2> /dev/null";
     int status = system(cmd.c_str());
     REQUIRE(status != -1);
-    cmd = "rm -f " + pfs_filename.u8string() + " && mv " +
-          pfs_filename.u8string() + "_temp " + pfs_filename.u8string() +
+    cmd = "mv " + pfs_filename.u8string() + "_temp " + pfs_filename.u8string() +
           " 2> /dev/null";
     status = system(cmd.c_str());
     REQUIRE(status != -1);
